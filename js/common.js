7817ac918464ce0cef3dc2103cbeb326ef7fea64
slabnevitch <handofschpilrain@mail.ru>
2018-06-21 01:04:31 +0500

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,53 @@
+$(function() {
+
+		$(document).ready(function() {
+				var $carousel = $('.carousel').slick({
+					autoplay: true,
+					dots: false,
+					draggable: false,
+					fade: true
+				});
+
+				$('.carousel-nav__item').click(function() {
+					$carousel.slick('slickGoTo', $(this).index());
+				});
+		});
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+	
+});
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+});
